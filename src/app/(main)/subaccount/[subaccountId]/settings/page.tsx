59bdbf84963cd680b0fa0bb2786579638afedcd2
dev-mahfuj80 <mahfuj@nexstack.sg>
2025-06-16 13:13:52 +0600

    feat: add navigation, infobar and settings pages for agency and subaccount

--- src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/settings/page.tsx
@@ -1,36 +1,36 @@
-import SubAccountDetails from '@/components/forms/subaccount-details'
-import UserDetails from '@/components/forms/user-details'
-import BlurPage from '@/components/global/blur-page'
-import { db } from '@/lib/db'
-import { currentUser } from '@clerk/nextjs'
-import React from 'react'
+import SubAccountDetails from "@/components/forms/subaccount-details";
+import UserDetails from "@/components/forms/user-details";
+import BlurPage from "@/components/global/blur-page";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import React from "react";
 
 type Props = {
-  params: { subaccountId: string }
-}
+  params: { subaccountId: string };
+};
 
 const SubaccountSettingPage = async ({ params }: Props) => {
-  const authUser = await currentUser()
-  if (!authUser) return
+  const authUser = await currentUser();
+  if (!authUser) return;
   const userDetails = await db.user.findUnique({
     where: {
       email: authUser.emailAddresses[0].emailAddress,
     },
-  })
-  if (!userDetails) return
+  });
+  if (!userDetails) return;
 
   const subAccount = await db.subAccount.findUnique({
     where: { id: params.subaccountId },
-  })
-  if (!subAccount) return
+  });
+  if (!subAccount) return;
 
   const agencyDetails = await db.agency.findUnique({
     where: { id: subAccount.agencyId },
     include: { SubAccount: true },
-  })
+  });
 
-  if (!agencyDetails) return
-  const subAccounts = agencyDetails.SubAccount
+  if (!agencyDetails) return;
+  const subAccounts = agencyDetails.SubAccount;
 
   return (
     <BlurPage>
@@ -49,7 +49,7 @@ const SubaccountSettingPage = async ({ params }: Props) => {
         />
       </div>
     </BlurPage>
-  )
-}
+  );
+};
 
-export default SubaccountSettingPage
+export default SubaccountSettingPage;

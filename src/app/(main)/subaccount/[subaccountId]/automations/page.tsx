b479cbde4397842c3b1ba54eb193e0bbbbfba495
dev-mahfuj80 <mahfuj@nexstack.sg>
2025-06-20 12:00:39 +0600

    feat: add basic automation page with blur effect wrapper

---
+++ src/app/(main)/subaccount/[subaccountId]/automations/page.tsx
@@ -0,0 +1,12 @@
+//create automation page
+import BlurPage from "@/components/global/blur-page";
+
+const Page = () => {
+  return (
+    <BlurPage>
+      <h1>Automation</h1>
+    </BlurPage>
+  );
+};
+
+export default Page;
